386024fb8da10b3af2ff9928972eefd1891dc894
itai <itaiz@gene.co.zw>
2021-07-18 13:09:23 +0200

    new

--- src/app/agent/agent.module.ts
+++ src/app/agent/agent.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { AgentRoutingModule } from './agent-routing.module';
 import { HomeComponent } from './home/home.component';
 import { RateuserComponent } from './rateuser/rateuser.component';
+import { ProfileComponent } from './profile/profile.component';
 
 
 @NgModule({
-  declarations: [HomeComponent, RateuserComponent],
+  declarations: [HomeComponent, RateuserComponent, ProfileComponent],
   imports: [
     CommonModule,
     AgentRoutingModule
